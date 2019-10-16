1ceaf57c3900c9a9363ba56ac46351c041f31a52
iths-anna-boberg <amboberg@gmail.com>
2019-10-16 13:00:28 +0200

    grund till stil och funktionalitet
    skapa lista av årtal

--- js/main.js
+++ js/main.js
@@ -3,3 +3,31 @@ async function getLaureates(){
     let listLaureates = await response.json()
     return listLaureates.results;
 }
+
+function createCheckYear(){
+    
+    let selectList = document.querySelector("#select-year")
+    let itemTest = document.querySelector("option.before");
+    let startYear = 1901;
+    let d = new Date();
+    let endYear = d.getFullYear();
+    
+    while (startYear <= endYear) {
+        let yearOption = itemTest.cloneNode(true);
+        yearOption.setAttribute("value",startYear);
+        yearOption.classList.remove("before");
+        yearOption.classList.add("option-list");
+        yearOption.innerText = startYear;
+        selectList.append(yearOption);
+        startYear++;
+    }
+
+}
+
+createCheckYear();
+
+async function renderLaureatesByYear(){
+    let list = await getLaureates();
+    
+
+}
